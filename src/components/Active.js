27d8c87f9cf6658aa268205e6c08266629b0009e
Aldren Alangcao <aaalangcao@gmail.com>
2019-10-26 20:33:58 +0800

    Added component: Active

---
+++ src/components/Active.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Active = ({ isActive }) => {
+    const activeClass = isActive ? "active" : "inactive";
+    const activeText = isActive ? "Active" : "Inactive";
+    return (
+        <div className="cell-active">
+            <span className={`is-active-dot ${activeClass}`} /> <span className="is-active-text"> {activeText} </span>
+        </div >
+    )
+}
+
+Active.propTypes = {
+    isActive: PropTypes.bool
+}
+
+Active.defaultProps = {
+    isActive: false
+}
+
+export default Active;
