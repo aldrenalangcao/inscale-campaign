e36478291bba7b2174a1be25b7717049479400c8
Aldren Alangcao <aldren.alangcao.ext@axa-asiassc.com>
2019-10-28 17:25:55 +0800

    Updates

---
+++ precache-manifest.2dbf396ac492e391680777c77568ea7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37faf55f11f2297fb8ec08b9a1907acf",
+    "url": "/inscale-campaign/index.html"
+  },
+  {
+    "revision": "705d4a42e044a6be749f",
+    "url": "/inscale-campaign/static/css/main.97804818.chunk.css"
+  },
+  {
+    "revision": "b1219fcf2edc70647c95",
+    "url": "/inscale-campaign/static/js/2.671b72b4.chunk.js"
+  },
+  {
+    "revision": "705d4a42e044a6be749f",
+    "url": "/inscale-campaign/static/js/main.460b836e.chunk.js"
+  },
+  {
+    "revision": "4a83cdfa34fdc783d864",
+    "url": "/inscale-campaign/static/js/runtime-main.0e85b8f2.js"
+  }
+]);
